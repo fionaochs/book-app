eaed81ecf13e0e69541da7165742cd7925f20717
Fiona Ochs <fiona.oaks@gmail.com>
2020-03-25 14:47:30 -0700

    author tests passing

--- db/seed.js
+++ db/seed.js
@@ -6,13 +6,12 @@ module.exports = async({ authorsToCreate = 10, booksToCreate = 100 } = {}) => {
 
   const authorOption = ['@shakespeare', '@ramDass', '@edgarAllenPoe'];
   const authors = await Author.create([...Array(authorsToCreate)].map(() => ({
-    author: chance.pickone(authorOption),
-    text: chance.sentence()
+    name: chance.pickone(authorOption),
+    genre: chance.sentence()
   })));
 
   await Book.create([...Array(booksToCreate)].map(() => ({
-    tweetId: chance.pickone(authors)._id,
-    author: chance.pickone(authorOption),
+    authorId: chance.pickone(authors)._id,
     text: chance.sentence()
   })));
 };
